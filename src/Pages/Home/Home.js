f8ed3b7207cd8c2713cbd754c984b4466ff19e74
a-haseeb98 <haseeb.dar.dev@gmail.com>
2022-08-03 00:59:30 +0500

    adsds

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,17 +6,25 @@ import Note from "../../Components/Home/Note";
 import HeroScreen from "../../Components/Home/HeroScreen";
 import Cover from "../../Components/Home/cover";
 import Navbar from "../../Components/Home/header";
+import useWindowSize from '../../Utilities/Hooks/useWindowSize';
 
 
 const Home = () => {
+  const { windowWidth } = useWindowSize();
+
   return (
     <>
-      <Navbar/>
-      <Cover/>
-      <WhyDoes/>
-      <SixtyFour/>
-      <Auctor/>
-      <Note/>
+      <Navbar />
+      {
+        windowWidth > 895.98 ?
+          <Cover />
+          :
+          <HeroScreen />
+      }
+      <WhyDoes />
+      <SixtyFour />
+      <Auctor />
+      <Note />
     </>
   );
 };
